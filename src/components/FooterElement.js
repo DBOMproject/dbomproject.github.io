263901297bf126a8925b2993084cda834a4e29b8
irajeshegde <33092803+irajeshegde@users.noreply.github.com>
2023-01-17 01:58:29 +0530

    update footer section, top to bottom, layout

--- src/components/FooterElement.js
+++ src/components/FooterElement.js
@@ -1,12 +1,20 @@
 import * as React from "react"
+import { Box, Image } from "@chakra-ui/react"
+import tlfLogo from "../images/linux-foundation-vert-white.svg"
 
 const FooterElement = () => (
-    <>
-        <div>
-
-        </div>
-    </>
+  <>
+    <Box
+      w={"100%"}
+      h={"8.75rem"}
+      bg={"brand.500"}
+      display={"flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
+      <Image src={tlfLogo} alt="the-linux-foundation-logo"></Image>
+    </Box>
+  </>
 )
-
 
 export default FooterElement
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,31 +5,71 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 
 import HeaderElement from "./HeaderElement"
-import "./layout.css"
+import "../styles/layout.css"
 import "@fontsource/open-sans"
-import FooterElement from "./FooterElement"; // Defaults to weight 400 with normal variant.
+import FooterElement from "./FooterElement" // Defaults to weight 400 with normal variant.
+import { ArrowUpIcon } from "@chakra-ui/icons"
+import { Box, IconButton } from "@chakra-ui/react"
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false)
 
-    return (
-        <>
-            <HeaderElement/>
-            <div
-                style={{
-                    margin: `0 auto`,
-                    maxWidth: `var(--size-content)`,
-                    padding: `var(--size-gutter)`,
-                }}
-            >
-                <main>{children}</main>
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }
 
-            </div>
-            <FooterElement/>
-        </>
-    )
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 200) {
+        setIsVisible(true)
+      } else {
+        setIsVisible(false)
+      }
+    }
+
+    window.addEventListener("scroll", toggleVisibility)
+
+    return () => window.removeEventListener("scroll", toggleVisibility)
+  }, [])
+
+  return (
+    <>
+      <HeaderElement />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: `var(--size-content)`,
+          padding: `var(--size-gutter)`,
+        }}
+      >
+        <main>{children}</main>
+      </div>
+      <FooterElement />
+      {isVisible && (
+        <Box
+          onClick={scrollToTop}
+          position="fixed"
+          bottom={["1rem", "1.25rem", "1.25rem", "1.25rem"]}
+          right={["1rem", "1.25rem", "2rem", "2.25rem"]}
+          zIndex={3}
+        >
+          <IconButton
+            icon={<ArrowUpIcon color={"white"} />}
+            bgColor="dbomGreen.500"
+            variant="solid"
+            aria-label="Scroll To Top"
+            borderRadius={"20rem"}
+          />
+        </Box>
+      )}
+    </>
+  )
 }
 
 export default Layout
