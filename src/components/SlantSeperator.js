b8309fdec5faa5c2cd2112918706d9fed0b9a68b
amithkk <amithkumaran@gmail.com>
2023-01-17 15:56:46 +0530

    Use transform instead of linear gradient for seperator.

--- src/components/SlantSeperator.js
+++ src/components/SlantSeperator.js
@@ -3,16 +3,25 @@ import { Box } from "@chakra-ui/react"
 import { css } from "@emotion/react"
 
 const SlantSeperator = () => (
-  <>
-    <Box
-      css={css`
-        width: 100%;
-        height: 100px;
-        background: linear-gradient(to right bottom, #ffffff 49%, #f1f6f9 50%),
-          linear-gradient(-50deg, #ffffff 16px, #fff 0);
-      `}
-    ></Box>
-  </>
+  <Box
+    css={css`
+      width: 100%;
+      height: 10em;
+      overflow: hidden;
+      position: relative;
+    `}
+    _after={{
+      content: '" "',
+      position: "absolute",
+      height: "10em",
+      boxShadow: "rgb(147 154 161 / 20%) 4px 1px 5px 10px",
+      bottom: "-5em",
+      width: "100%",
+      display: "block",
+      transform: "rotate(-4deg)",
+      backgroundColor: "var(--chakra-colors-brand-100)",
+    }}
+  ></Box>
 )
 
 export default SlantSeperator
